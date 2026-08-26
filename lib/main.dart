40af9790536951d016b44568967dcec34f1b6358
ossohouangestevieclotaire-boop <ossohouangestevieclotaire@gmail.com>
2026-08-26 16:30:50 +0000

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/robot_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://vxaglbaqfpxitbdimeqj.supabase.co',
+    anonKey: 'sb_publishable_otC4yztme4tqkL9BA8FM5Q_APYQrtGe',
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Makbine Robot',
+      home: const RobotTransfertScreen(),
+      theme: ThemeData(primarySwatch: Colors.amber),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
