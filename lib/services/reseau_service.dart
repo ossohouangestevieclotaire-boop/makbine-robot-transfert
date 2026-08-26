3eeedac146055b8407a8479d2eaedf527ec3946f
ossohouangestevieclotaire-boop <ossohouangestevieclotaire@gmail.com>
2026-08-26 16:26:51 +0000

    Delete lib directory

--- lib/services/reseau_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class ReseauService {
-  Future<bool> estConnecte() async {
-    final resultats = await Connectivity().checkConnectivity();
-    return !resultats.contains(ConnectivityResult.none);
-  }
-}
